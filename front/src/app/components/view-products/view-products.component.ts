0706c027031e2f3ba72ffadc2466307491f3c3f7
OctaneMv33 <112354808+OctaneMv33@users.noreply.github.com>
2023-10-09 21:33:22 -0300

    Descargar reporte listo
    Ya funciona el descargar reporte, hay que afinar un detalle sobre la cantidad de ítems que se observan, ya que no aparecen todos lo que yo tengo. Ej: de 97 artículos, solo se ven 45 y no los restantes en el pdf.

--- front/src/app/components/view-products/view-products.component.ts
+++ front/src/app/components/view-products/view-products.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { BackendService } from 'src/app/services/backend.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
 @Component({
   selector: 'app-view-products',
   templateUrl: './view-products.component.html',
@@ -7,7 +10,9 @@ import { BackendService } from 'src/app/services/backend.service';
 })
 export class ViewProductsComponent {
   reporte: any | undefined
-  constructor(private backendService:BackendService ){}
+  constructor(private backendService:BackendService ){
+    
+  }
 
   getReporteApi(){
     this.backendService.getReporte().subscribe(data=>{
@@ -19,5 +24,38 @@ export class ViewProductsComponent {
     this.backendService.postReport().subscribe(data=>{
       this.reporte = data
     })
+  }
+
+  descargarPDF(): void{
+    const documento = new jsPDF('p', 'pt', 'a4');
+    const informacion: any  = document.getElementById('htmlData');
+    const opciones = {
+      background: 'white',
+      scale: 3
+    }
+
+    html2canvas(informacion, opciones).then((canvas) =>{
+      const img = canvas.toDataURL('image/PNG');
+
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (documento as any).getImageProperties(img)
+      const pdfWidth = documento.internal.pageSize.getWidth() - 2 *bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      documento.addImage(
+        img,
+        'PNG',
+        bufferX,
+        bufferY,
+        pdfWidth,
+        pdfHeight,
+        undefined,
+        'FAST'
+      );
+      return documento;
+    })
+      .then((docResult) => {
+        docResult.save(`Reporte_Fecha(${new Date().toISOString()}).pdf`);
+      })
   }
 }

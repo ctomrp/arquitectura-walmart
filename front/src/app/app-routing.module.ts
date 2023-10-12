99dae0e2fd9ace23988d7de6b9820fd053c81b9c
Raksaza <131925590+Raksaza@users.noreply.github.com>
2023-10-12 16:44:09 -0300

    pequeñoCambioFront.html
    se hace un leve cambio en front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewProductsComponent } from './components/view-products/view-products.component';
+import { ComponentsComponent } from './visualizarReporte/components/components.component';
 
 const routes: Routes = [
-  {path: '', component: ViewProductsComponent}
+  {path: '', component: ViewProductsComponent, },
+  {path: '', component: ComponentsComponent, }
 ];
 
 @NgModule({
